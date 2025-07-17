e526d205aacca9c49a68656a7ebd02ae8a84d5e9
vsblanco <sly.victorb@gmail.com>
2025-07-17 18:23:05 -0400

    Update taskpane.js

--- taskpane.js
+++ taskpane.js
@@ -6,34 +6,28 @@
 // The initialize function must be run each time a new page is loaded.
 Office.onReady((info) => {
   if (info.host === Office.HostType.Excel) {
-    // Assign click event handlers for the buttons.
+    // Assign click event handlers for the buttons in the taskpane.
     document.getElementById("insert-text-button").onclick = insertText;
-    document.getElementById("highlight-button").onclick = toggleHighlight;
+    
     console.log("Add-in is ready to use in Excel!");
   }
 });
 
 /**
  * Highlights the selected cell in yellow. If the cell is already yellow, it removes the highlight.
+ * This function is called by a ribbon button.
+ * @param {Office.AddinCommands.Event} event The event object passed from the ribbon button.
  */
-async function toggleHighlight() {
+async function toggleHighlight(event) {
   try {
     await Excel.run(async (context) => {
-      // Get the currently selected range.
       const range = context.workbook.getSelectedRange();
-      
-      // Load the 'color' property from the range's format/fill.
       range.format.fill.load("color");
-
       await context.sync();
 
-      // Check the current color of the cell.
-      // The color is returned in the format "#RRGGBB".
       if (range.format.fill.color === "#FFFF00") {
-        // If the color is yellow, clear the fill to remove the highlight.
         range.format.fill.clear();
       } else {
-        // If the color is not yellow, set it to yellow.
         range.format.fill.color = "yellow";
       }
 
@@ -45,8 +39,12 @@ async function toggleHighlight() {
         console.error("Debug info: " + JSON.stringify(error.debugInfo));
     }
   }
+  
+  // Important: This tells the Office application that the function has completed its execution.
+  if (event) {
+    event.completed();
+  }
 }
-
 
 /**
  * Writes the string "Hello Excel!" into the currently selected cell in the worksheet.
@@ -67,4 +65,10 @@ async function insertText() {
         console.error("Debug info: " + JSON.stringify(error.debugInfo));
     }
   }
+}
+
+// This is a special function that Office calls to register ribbon button commands.
+// It maps the function name from the manifest to the actual JavaScript function.
+if (typeof Office.actions !== 'undefined') {
+  Office.actions.associate("toggleHighlight", toggleHighlight);
 }
