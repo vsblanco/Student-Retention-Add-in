66eba83e4e2e7396fb5ea9e5824f419a112187aa
vsblanco <sly.victorb@gmail.com>
2025-07-31 16:48:08 -0400

    Update commands.js

--- commands/commands.js
+++ commands/commands.js
@@ -6,8 +6,15 @@ import { openImportDialog } from './import.js';
 import { openCreateLdaDialog } from './lda.js';
 import { toggleHighlight, transferData } from './actions.js';
 
+// This function is required for the Analytics button, even if it does nothing,
+// because the manifest uses a ShowTaskpane action.
+function openAnalyticsPane(event) {
+    event.completed();
+}
+
 // Register ribbon button commands
 Office.actions.associate("toggleHighlight", toggleHighlight);
 Office.actions.associate("openImportDialog", openImportDialog);
 Office.actions.associate("transferData", transferData);
 Office.actions.associate("openCreateLdaDialog", openCreateLdaDialog);
+Office.actions.associate("openAnalyticsPane", openAnalyticsPane);
