0c79d73878e00f5b453275f5307394d351464a62
vsblanco <sly.victorb@gmail.com>
2025-10-02 19:20:12 +0000

    added REACT.

---
+++ retention/src/App.jsx
@@ -0,0 +1,15 @@
+// Timestamp: 2025-10-02 03:20 PM | Version: 1.0.0
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Student Retention Settings</h1>
+        <p>This is the new settings page, powered by React!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
