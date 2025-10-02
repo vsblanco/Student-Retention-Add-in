a3402bc406a4b7c9b5ef707e5e2221522451830a
vsblanco <sly.victorb@gmail.com>
2025-10-02 15:25:18 +0000

    added email settings

--- settings/constants.js
+++ settings/constants.js
@@ -1,8 +1,10 @@
-// Timestamp: 2025-10-02 10:04 AM | Version: 1.0.0
+// Timestamp: 2025-10-02 11:23 AM | Version: 1.1.0
 const CONSTANTS = {
     // Keys for Office.context.document.settings
     SETTINGS_KEYS: {
-        APP: "studentRetentionSettings"
+        APP: "studentRetentionSettings",
+        EMAIL_TEMPLATES: "emailTemplates",
+        CUSTOM_PARAMS: "customParams"
     },
     // Names of worksheets used in the add-in
     SHEET_NAMES: {
